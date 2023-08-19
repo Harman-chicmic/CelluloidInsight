8f46b53f7e982cb18ea6efadfee4731563f44fe3
astroUday <uday.sharma@chicmic.co.in>
2023-08-19 17:59:34 +0530

    Updates

---
+++ src/main/java/com/example/CelluloidInsight/configurations/ProducerConfig.java
@@ -0,0 +1,32 @@
+package com.example.CelluloidInsight.configurations;
+
+import com.example.CelluloidInsight.Dto.ProducerDto;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProducerConfig {
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootStrapService;
+    @Bean
+    public ProducerFactory<Integer, ProducerDto> producerFactory() {
+        Map<String, Object> prop = new HashMap<>();
+        prop.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapService);
+        prop.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        prop.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ProducerDtoSerializer.class);
+        return new DefaultKafkaProducerFactory<>(prop);
+    }
+
+    @Bean
+    public KafkaTemplate<Integer, ProducerDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
---
+++ src/main/java/com/example/CelluloidInsight/services/ProducerService.java
@@ -0,0 +1,61 @@
+package com.example.CelluloidInsight.services;
+
+import com.example.CelluloidInsight.Dto.ProducerDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@EnableKafka
+@Slf4j
+@RequiredArgsConstructor
+public class ProducerService {
+    @Autowired
+    private KafkaTemplate<Integer,ProducerDto> kafkaTemplate;
+
+    @Value("${kafka.topic-create}")
+    private final String TOPIC_CREATE;
+    @Value("${kafka.topic-update}")
+    private final String TOPIC_UPDATE;
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper=new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        return objectMapper;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> produceCreateRatingMessage(ProducerDto producerDto) {
+
+        ProducerRecord<Integer,ProducerDto> producerRecord = buildProducerRecord(producerDto,TOPIC_CREATE);        // can later see if need seperate keys
+        kafkaTemplate.send(producerRecord);     // send message
+        return new CompletableFuture<>();
+    }
+    public CompletableFuture<SendResult<Integer, String>> produceUpdateRatingMessage(ProducerDto producerDto) {
+
+        ProducerRecord<Integer,ProducerDto> producerRecord = buildProducerRecord(producerDto,TOPIC_UPDATE);        // can later see if need seperate keys
+        kafkaTemplate.send(producerRecord);     // send message
+        return new CompletableFuture<>();
+    }
+    private ProducerRecord<Integer, ProducerDto> buildProducerRecord(ProducerDto value, String topic) {
+
+//        List<Header> recordHeaders = List.of(new RecordHeader("authentication", "Bearer 1234567890".getBytes()));
+        return new ProducerRecord<>(topic, value);
+    }
+}
